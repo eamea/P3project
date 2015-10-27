d5010ba7581f8c1568013c9b2c60860811786d08
eamea <eustru14@student.aau.dk>
2015-10-27 09:33:29 +0100

    Created a Recognizer class (still empty).

---
+++ P3project/P3project/Recognizer.cpp
@@ -0,0 +1,7 @@
+#include "Recognizer.h"
+
+
+Recognizer::Recognizer()
+{
+	//yee imma recognize some shit up in here
+}
---
+++ P3project/P3project/Recognizer.h
@@ -0,0 +1,7 @@
+#pragma once
+class Recognizer
+{
+public:
+	Recognizer();
+};
+
