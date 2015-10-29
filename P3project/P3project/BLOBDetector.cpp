e797b9c3c2fb82ef674c35bb7d935806f39dec32
WhiteBatz <patrick.lausen@gmail.com>
2015-10-29 12:26:51 +0100

    Added grass fire algorithm to the BLOBDetector class

--- P3project/P3project/BLOBDetector.cpp
+++ P3project/P3project/BLOBDetector.cpp
@@ -1,11 +1,48 @@
 #include "BLOBDetector.h"
 
 
-BLOBDetector::BLOBDetector()
-{
+BLOBDetector::BLOBDetector(Mat thresImg){
+	Mat img = thresImg;
 }
 
-
-BLOBDetector::~BLOBDetector()
-{
+void BLOBDetector::BLOBDetection(){
+	for (size_t row = 0; row < img.row; row++){
+		for (size_t col = 0; col < img.col; col++){
+			if (img.at<unsigned char>(row, col) == 255){
+				grassFire(row, col);
+				BLOB = BLOB + 1;
+			}
+			
+		}
+	}
 }
+
+void BLOBDetector::grassFire(size_t row, size_t col){
+	img.at<unsigned char>(row, col) = 0;
+	BLOB.at<unsigned char>(row, col) = object;
+	if (img.at<unsigned char>(row + 1, col) == 255){
+		grassFire(row + 1, col);
+	}
+	if (img.at<unsigned char>(row + 1, col + 1) == 255){
+		grassFire(row + 1, col + 1);
+	}
+	if (img.at<unsigned char>(row, col + 1) == 255){
+		grassFire(row, col + 1);
+	}
+	if (img.at<unsigned char>(row - 1, col + 1) == 255){
+		grassFire(row - 1, col + 1);
+	}
+	if (img.at<unsigned char>(row - 1, col) == 255){
+		grassFire(row - 1, col);
+	}
+	if (img.at<unsigned char>(row -1, col - 1) == 255){
+		grassFire(row - 1, col - 1);
+	}
+	if (img.at<unsigned char>(row, col - 1) == 255){
+		grassFire(row, col - 1);
+	}
+	if (img.at<unsigned char>(row + 1, col - 1) == 255){
+		grassFire(row + 1, col - 1);
+	}
+}
+
--- P3project/P3project/BLOBDetector.h
+++ P3project/P3project/BLOBDetector.h
@@ -1,8 +1,22 @@
 #pragma once
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
 class BLOBDetector
 {
 public:
-	BLOBDetector();
-	~BLOBDetector();
+	BLOBDetector(Mat thresImg);
+	void BLOBDetection();
+	void grassFire(size_t row, size_t col); //temporary method for detecting the BLOB's
+
+private:
+	Mat img;
+	vector < KeyPoint >  BLOBVectors;
+	Mat BLOB;
+	int object = 1;
 };
 
