bb99dd278d5b09156515d578c984f36b7d20177a
LeJooks <jpou14@student.aau.dk>
2015-11-03 11:16:36 +0100

    Recognizer: Wrote code for BLOBAnalyze() and recognize.

--- P3project/P3project/Recognizer.cpp
+++ P3project/P3project/Recognizer.cpp
@@ -1,6 +1,6 @@
 #include "Recognizer.h"
 
-
+//TODO Uses getters from main function to set the threshold images.
 Recognizer::Recognizer(){
 	//blueThreshImg = getBlueThreshImg();
 	//greenThreshImg = getGreenThreshImg();
@@ -9,9 +9,39 @@ Recognizer::Recognizer(){
 	//yellowThreshImg = getYellowThreshImg();
 }
 
+//BLOB analyzes the relevant threshimages based on the char input.
 void Recognizer::BLOBAnalyze(char x)
 {
-
+	switch (x)
+	{
+	case 'a':
+		BLOBBlueImg = detector.BLOBDetection(blueThreshImg);
+		BLOBYellowImg = detector.BLOBDetection(yellowThreshImg);
+		break;
+	case 'b':
+		BLOBRedImg = detector.BLOBDetection(redThreshImg);
+		BLOBYellowImg = detector.BLOBDetection(yellowThreshImg);
+		break;
+	case 'f':
+		BLOBBlueImg = detector.BLOBDetection(blueThreshImg);
+		BLOBGreenImg = detector.BLOBDetection(greenThreshImg);
+		BLOBRedImg = detector.BLOBDetection(redThreshImg);
+		BLOBYellowImg = detector.BLOBDetection(yellowThreshImg);
+		break;
+	case 'l':
+		BLOBBlueImg = detector.BLOBDetection(blueThreshImg);
+		BLOBRedImg = detector.BLOBDetection(redThreshImg);
+		BLOBYellowImg = detector.BLOBDetection(yellowThreshImg);
+		break;
+	case 's':
+		BLOBBlueImg = detector.BLOBDetection(blueThreshImg);
+		BLOBYellowImg = detector.BLOBDetection(yellowThreshImg);
+		break;
+	case 't':
+		BLOBBlueImg = detector.BLOBDetection(blueThreshImg);
+		BLOBYellowImg = detector.BLOBDetection(yellowThreshImg);
+		break;
+	}
 }
 
 void Recognizer::extractFeatures(char x)
@@ -19,12 +49,17 @@ void Recognizer::extractFeatures(char x)
 
 }
 
-void Recognizer::compareFeatures(char x)
+bool Recognizer::compareFeatures(char x)
 {
 
 }
 
+//Runs the functions in order and returns a bool based on whether it found the sign for the input char.
 bool Recognizer::recognize(char x)
 {
-
+	BLOBAnalyze(x);
+	extractFeatures(x);
+	
+	return compareFeatures(x);
+	
 }
--- P3project/P3project/Recognizer.h
+++ P3project/P3project/Recognizer.h
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/features2d.hpp"
+#include "BLOBDetector.h"
 
 using namespace std;
 using namespace cv;
@@ -10,16 +11,26 @@ class Recognizer
 {
 public:
 	Recognizer();
-	void BLOBAnalyze(char x);
+	void BLOBAnalyze(char x);		//BLOB analyzes the relevant threshimages based on the char input.
 	void extractFeatures(char x);
-	void compareFeatures(char x);
-	bool recognize(char x);
+	bool compareFeatures(char x);
+	bool recognize(char x);			//Runs the functions in order and returns a bool based on whether it found the sign for the input char.
 private:
+	//Matrices to hold the thresholded images.
 	Mat blueThreshImg;
 	Mat greenThreshImg;
 	//Mat pinkThreshImg;
 	Mat redThreshImg;
 	Mat yellowThreshImg;
+
+	//Matrices to hold the BLOB analysed images.
+	Mat BLOBBlueImg;
+	Mat BLOBGreenImg;
+	//Mat BLOBPinkImg;
+	Mat BLOBRedImg;
+	Mat BLOBYellowImg;
+
+	BLOBDetector detector;
 	
 	//Need variables for basic features.
 
