ad476a31a9ac19f826cf1d27270dcca77b5e32f7
eamea <eustru14@student.aau.dk>
2015-11-20 14:07:36 +0100

    .... added the Evaluator files, oops.

---
+++ P3project/P3project/Evaluator.cpp
@@ -0,0 +1,37 @@
+#include "Evaluator.h"
+
+
+Evaluator::Evaluator()
+{
+
+}
+
+void Evaluator::startTimer(char letter){
+	currentLetter = letter;
+	startTime = clock();
+}
+
+
+void Evaluator::stopTimer(string str){
+	filename = str;
+	endTime = clock();
+
+	clockTicsTaken = endTime - startTime;
+	timeInSeconds = clockTicsTaken / (double)CLOCKS_PER_SEC;
+
+	writeToFile(filename);
+}
+
+void Evaluator::writeToFile(string filename){
+	outputFile.open(filename);
+	dataToSend << "\nSign '" << currentLetter << "': clockTicsTaken = " << clockTicsTaken << ". timeInSeconds = " << timeInSeconds << ".";
+	outputFile << dataToSend.str();
+	outputFile.close();
+}
+
+
+/*clock_t startTime = clock();
+doSomeOperation();
+clock_t endTime = clock();
+clock_t clockTicksTaken = endTime - startTime;
+double timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;*/
---
+++ P3project/P3project/Evaluator.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+class Evaluator
+{
+public:
+	Evaluator();
+	void startTimer(char letter);
+	void stopTimer(string filename);
+private:
+	void writeToFile(string filename);
+
+	char currentLetter;
+	stringstream dataToSend;
+	ofstream outputFile;
+	string filename;
+	clock_t startTime;
+	clock_t endTime;
+	clock_t clockTicsTaken;
+	double timeInSeconds;
+
+};
+
+
+/*clock_t startTime = clock();
+doSomeOperation();
+clock_t endTime = clock();
+clock_t clockTicksTaken = endTime - startTime;
+double timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;*/
+
