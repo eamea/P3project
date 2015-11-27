c1c4570401da7a30afac1d1de3e951976653da44
WhiteBatz <patrick.lausen@gmail.com>
2015-11-27 13:45:54 +0100

    Fully implemented the saving of videos for both BLOB and Threshold videos. Works for both glove 1 and 2

--- P3project/P3project/Evaluator.cpp
+++ P3project/P3project/Evaluator.cpp
@@ -4,11 +4,71 @@ Evaluator::Evaluator(){
 }
 
 //start the timer for the parsed char
-void Evaluator::startTimer(char letter, Mat img){
+void Evaluator::startTimer(char letter, Mat img, int gloveNumber){
 	currentLetter = letter;											//saving parsed char
 	timerHasStarted = true;											//"starting" timer
 	startTime = clock();											//check the current clock_tic and set startTime as this
-	videoWriter.open("C:/Users/Patrick/Desktop/5seconds.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), true); //the videoWriter opens the specified file, descripes the type each frame is, decides the frame reate, declares the size of the video, and wether or not it's a colour picture
+	NormalVid.open("C:/Users/Patrick/Desktop/Vids/NormalVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), true); //the videoWriter opens the specified file, descripes the type each frame is, decides the frame reate, declares the size of the video, and wether or not it's a colour picture
+	if (gloveNumber == 1){
+		switch (letter){
+		case 'a':
+			blueBLOBVid.open("C:/Users/Patrick/Desktop/Vids/BlueBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			blueTreshVid.open("C:/Users/Patrick/Desktop/Vids/BlueThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			break;
+		case 'b':
+			redBLOBVid.open("C:/Users/Patrick/Desktop/Vids/RedBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			redTreshVid.open("C:/Users/Patrick/Desktop/Vids/RedThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			break;
+		case 'f':
+			blueBLOBVid.open("C:/Users/Patrick/Desktop/Vids/BlueBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			blueTreshVid.open("C:/Users/Patrick/Desktop/Vids/BlueThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			redBLOBVid.open("C:/Users/Patrick/Desktop/Vids/RedBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			redTreshVid.open("C:/Users/Patrick/Desktop/Vids/RedThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			break;
+		case 'l':
+			blueBLOBVid.open("C:/Users/Patrick/Desktop/Vids/BlueBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			blueTreshVid.open("C:/Users/Patrick/Desktop/Vids/BlueThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			redBLOBVid.open("C:/Users/Patrick/Desktop/Vids/RedBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			redTreshVid.open("C:/Users/Patrick/Desktop/Vids/RedThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			break;
+		case 's':
+			blueBLOBVid.open("C:/Users/Patrick/Desktop/Vids/BlueBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			blueTreshVid.open("C:/Users/Patrick/Desktop/Vids/BlueThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			break;
+		case 't':
+			blueBLOBVid.open("C:/Users/Patrick/Desktop/Vids/BlueBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			blueTreshVid.open("C:/Users/Patrick/Desktop/Vids/BlueThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+			break;
+		default:
+			cout << "Evaluator was not given a proper letter" << endl;
+		}
+	}
+	else if (gloveNumber == 2){
+		blueBLOBVid.open("C:/Users/Patrick/Desktop/Vids/BlueBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		blueTreshVid.open("C:/Users/Patrick/Desktop/Vids/BlueThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		greenBLOBVid.open("C:/Users/Patrick/Desktop/Vids/GreenBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		greenTreshVid.open("C:/Users/Patrick/Desktop/Vids/GreenThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		pinkBLOBVid.open("C:/Users/Patrick/Desktop/Vids/PinkBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		pinkTreshVid.open("C:/Users/Patrick/Desktop/Vids/PinkThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		redBLOBVid.open("C:/Users/Patrick/Desktop/Vids/RedBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		redTreshVid.open("C:/Users/Patrick/Desktop/Vids/RedThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		yellowBLOBVid.open("C:/Users/Patrick/Desktop/Vids/YellowBLOBVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+		yellowTreshVid.open("C:/Users/Patrick/Desktop/Vids/YellowThreshVid.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(img.cols, img.rows), false);
+	}
+	else
+		cout << "guess we're setting thresholds" << endl;
 }
 
 //stop the timer and write to the file with the parsed name
@@ -23,7 +83,17 @@ void Evaluator::stopTimer(string str){
 
 	timerHasStarted = false;										//"stopping" the timer
 
-	videoWriter.release();
+	NormalVid.release();											//stops the video streams
+	blueBLOBVid.release();
+	blueTreshVid.release();
+	greenBLOBVid.release();
+	greenTreshVid.release();
+	pinkBLOBVid.release();
+	pinkTreshVid.release();
+	redBLOBVid.release();
+	redTreshVid.release();
+	yellowBLOBVid.release();
+	yellowTreshVid.release();
 }
 
 //writing to the specified file
@@ -35,8 +105,90 @@ void Evaluator::writeToFile(string filename){
 }
 
 //save videoStream to file
-void Evaluator::saveVideo(Mat img){									
-		videoWriter.write(img);										//save the frame to the videoWriter
+void Evaluator::saveThreshVid(Mat img, Mat blueThreshImg, Mat greenThreshImg, Mat pinkThreshImg, Mat redThreshImg, Mat yellowThreshImg, char letter, int gloveNumber){
+		NormalVid.write(img);										//save the frame to the videoWriter
+		if (gloveNumber == 1){
+			switch (letter){
+			case 'a':
+				blueTreshVid.write(blueThreshImg);
+				yellowTreshVid.write(yellowThreshImg);
+				break;
+			case 'b':
+				redTreshVid.write(redThreshImg);
+				yellowTreshVid.write(yellowThreshImg);
+				break;
+			case 'f':
+				blueTreshVid.write(blueThreshImg);
+				redTreshVid.write(redThreshImg);
+				yellowTreshVid.write(yellowThreshImg);
+				break;
+			case 'l':
+				blueTreshVid.write(blueThreshImg);
+				redTreshVid.write(redThreshImg);
+				yellowTreshVid.write(yellowThreshImg);
+				break;
+			case 's':
+				blueTreshVid.write(blueThreshImg);
+				yellowTreshVid.write(yellowThreshImg);
+				break;
+			case 't':
+				blueTreshVid.write(blueThreshImg);
+				yellowTreshVid.write(yellowThreshImg);
+				break;
+			default:
+				cout << "Evaluator was not given a proper letter" << endl;
+			}
+		}
+		else{
+			blueTreshVid.write(blueThreshImg);
+			greenTreshVid.write(greenThreshImg);
+			pinkTreshVid.write(pinkThreshImg);
+			redTreshVid.write(redThreshImg);
+			yellowTreshVid.write(yellowThreshImg);
+		}
+}
+
+//save videoStream to file
+void Evaluator::saveBLOBVid(Mat blueBLOBImg, Mat greenBLOBImg, Mat pinkBLOBImg, Mat redBLOBImg, Mat yellowBLOBImg, char letter, int gloveNumber){
+	if (gloveNumber == 1){
+		switch (letter){
+		case 'a':
+			blueBLOBVid.write(blueBLOBImg);
+			yellowBLOBVid.write(yellowBLOBImg);
+			break;
+		case 'b':
+			redBLOBVid.write(redBLOBImg);
+			yellowBLOBVid.write(yellowBLOBImg);
+			break;
+		case 'f':
+			blueBLOBVid.write(blueBLOBImg);
+			redBLOBVid.write(redBLOBImg);
+			yellowBLOBVid.write(yellowBLOBImg);
+			break;
+		case 'l':
+			blueBLOBVid.write(blueBLOBImg);
+			redBLOBVid.write(redBLOBImg);
+			yellowBLOBVid.write(yellowBLOBImg);
+			break;
+		case 's':
+			blueBLOBVid.write(blueBLOBImg);
+			yellowBLOBVid.write(yellowBLOBImg);
+			break;
+		case 't':
+			blueBLOBVid.write(blueBLOBImg);
+			yellowBLOBVid.write(yellowBLOBImg);
+			break;
+		default:
+			cout << "Evaluator was not given a proper letter" << endl;
+		}
+	}
+	else{
+		blueBLOBVid.write(blueBLOBImg);
+		greenBLOBVid.write(greenBLOBImg);
+		pinkBLOBVid.write(pinkBLOBImg);
+		redBLOBVid.write(redBLOBImg);
+		yellowBLOBVid.write(yellowBLOBImg);
+	}
 }
 
 //set euclidean distance 
